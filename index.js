5caebd7f5a4fe6e3d329cdb8dbab1bf71e29cd4f
Miriam Villeda <102434406+miryv@users.noreply.github.com>
2023-11-23 08:33:14 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import { registerRootComponent } from "expo";
-
-import App from "./App";
-
-// Esta función carga la raiz de la aplicación a Expo Go, de forma que se pueda
-// Utilizar sin cargar la APK directamente al sistema
-registerRootComponent(App);
